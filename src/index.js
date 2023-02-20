2906cbe904ce86c18ad29ca7629a0ec1cd9591a0
rabbani4u <raabbani.iu31@gmail.com>
2023-02-21 01:16:31 +0600

    time zone

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function getTime() {
+  return new Date().toLocaleTimeString();
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div>
+      <h2>Current Time:</h2>
+      <h3>{getTime()}</h3>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
